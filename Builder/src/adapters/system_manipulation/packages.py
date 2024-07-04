af17225611e71f948abea1a526744dd37a809ae0
Your Name <you@example.com>
2024-07-04 23:27:48 +0300

    Добавить автоматическую подвязку флэшек

--- Builder/src/adapters/system_manipulation/packages.py
+++ Builder/src/adapters/system_manipulation/packages.py
@@ -1,22 +1,34 @@
 from src.application.configurator.enums import DriverTypeEnum
 
 PACMAN_DEPENDENCIES = [
-    "waybar", "wofi", "vscode", "noto-fonts", "noto-fonts-emoji", "ttf-linux-libertine", "ttf-jetbrains-mono-nerd",
-    "pavucontrol", "nwg-look", "mpv", "feh", "xdg-desktop-portal-wlr", "man", "obs-studio", "alacritty", "thunar",
-    "zsh", "gdb", "ninja", "gcc", "cmake", "meson", "libxcb", "xcb-proto", "xcb-util", "xcb-util-keysyms", "libxfixes",
-    "libx11", "libxcomposite", "xorg-xinput", "libxrender", "pixman", "wayland-protocols", "cairo", "pango", "seatd",
-    "libxkbcommon", "xcb-util-wm", "xorg-xwayland", "libinput", "libliftoff", "libdisplay-info", "cpio", "tomlplusplus",
-    "hyprlang", "hyprcursor", "hyprwayland-scanner", "xcb-util-errors", "curl", "wget", "micro", "hyprland",
-    "telegram-desktop", "fastfetch", 'hyprpaper', 'unzip', 'unrar', 'p7zip', 'openssh', 'cliphist'
+    'waybar', 'wofi', 'vscode', 'noto-fonts', 'noto-fonts-emoji', 'ttf-linux-libertine', 'ttf-jetbrains-mono-nerd',
+    'pavucontrol', 'nwg-look', 'mpv', 'feh', 'xdg-desktop-portal-wlr', 'man', 'obs-studio', 'alacritty', 'thunar',
+    'zsh', 'gdb', 'ninja', 'gcc', 'cmake', 'meson', 'libxcb', 'xcb-proto', 'xcb-util', 'xcb-util-keysyms', 'libxfixes',
+    'libx11', 'libxcomposite', 'xorg-xinput', 'libxrender', 'pixman', 'wayland-protocols', 'cairo', 'pango', 'seatd',
+    'libxkbcommon', 'xcb-util-wm', 'xorg-xwayland', 'libinput', 'libliftoff', 'libdisplay-info', 'cpio', 'tomlplusplus',
+    'hyprlang', 'hyprcursor', 'hyprwayland-scanner', 'xcb-util-errors', 'curl', 'wget', 'micro', 'hyprland',
+    'telegram-desktop', 'fastfetch', 'hyprpaper', 'unzip', 'unrar', 'p7zip', 'openssh', 'cliphist', 'udiskie'
 ]
 
 AUR_DEPENDENCIES = [
-    "google-chrome", "hyprshot",
+    'google-chrome', 'hyprshot',
 ]
 
 DRIVER_PACKAGES = {
     DriverTypeEnum.AMD_GPU_FREE: [
         'mesa', 'vulkan-radeon', 'libva-mesa-driver', 'lib32-libva-mesa-driver', 'lib32-mesa', 'lib32-vulkan-radeon',
         'vulkan-tools', 'vulkan-headers', 'vulkan-icd-loader', 'lib32-vulkan-icd-loader'
+    ],
+    DriverTypeEnum.NVIDIA_MAXWELL: [
+        'nvidia', 'nvidia-lts', 'lib32-nvidia-utils'
+    ],
+    DriverTypeEnum.NVIDIA_KEPLER: [
+        'nvidia-470xx-dkms'
+    ],
+    DriverTypeEnum.NVIDIA_TUNING: [
+        'nvidia-open'
+    ],
+    DriverTypeEnum.NVIDIA_TESLA: [
+        'nvidia-340xx-dkms'
     ]
 }
--- Builder/src/application/configurator/enums.py
+++ Builder/src/application/configurator/enums.py
@@ -2,13 +2,14 @@ from enum import Enum
 
 
 class DriverTypeEnum(str, Enum):
-    AMD_GPU_FREE = 'amd gpu free'
-    AMD_GPU_PRO = 'amd gpu pro'
-    NVIDIA_MAXWELL = 'nvidia Maxwell'
-    NVIDIA_MAXWELL_DKMS = 'nvidia Maxwell dkms'
-    NVIDIA_KEPLER = 'nvidia Kepler'
-    NVIDIA_NVCX_AND_NVDX = 'nvidia 400/500/600 series'
-    NVIDIA_TESLA = 'nvidia Telsa'
+    AMD_GPU_FREE = 'Amd gpu free'
+    AMD_GPU_PRO = 'Amd gpu pro'
+    NVIDIA_MAXWELL = 'Nvidia Maxwell'
+    NVIDIA_MAXWELL_DKMS = 'Nvidia Maxwell dkms'
+    NVIDIA_KEPLER = 'Nvidia Kepler'
+    NVIDIA_NVCX_AND_NVDX = 'Nvidia 400/500/600 series'
+    NVIDIA_TESLA = 'Nvidia Telsa'
+    NVIDIA_TUNING = 'Nvidia Tunning'
 
 
 class LoggerStatusEnum(str, Enum):
